bb11f887ba0ffb075520a3aaa1de72708bffac8b
Dominic <dominic.tobias@gmail.com>
2014-05-13 22:06:48 +0100

    Simplified exposing code (to window, amd and node). Moved function outside of loop and named it functionWrapper so the file is lint-free.

--- extnd.js
+++ extnd.js
@@ -2,30 +2,6 @@
 (function() {
 	'use strict';
 
-	// Used to determine if values are of the language type Object
-	var objectTypes = {
-		'function': true,
-		'object': true
-	};
-
-	// Used as a reference to the global object.
-	var root = (objectTypes[typeof window] && window) || this;
-
-	// Detect free variable `exports`.
-	var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
-
-	// Detect free variable `module`.
-	var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
-
-	// Detect free variable `global` from Node.js or Browserified code and use it as `root`.
-	var freeGlobal = freeExports && freeModule && typeof global === 'object' && global;
-	if (freeGlobal && (freeGlobal.global === freeGlobal || freeGlobal.window === freeGlobal || freeGlobal.self === freeGlobal)) {
-		root = freeGlobal;
-	}
-
-	// Detect the popular CommonJS extension `module.exports`.
-	var moduleExports = freeModule && freeModule.exports === freeExports && freeExports;
-
 	// Throw error if trying to call a super that doesn't exist.
 	var unImplementedSuper = function(method) {
 		throw 'Super does not implement this method: ' + method;
@@ -34,19 +10,21 @@
 	// Used to test if the method has a super.
 	var superTest = /\b_super\b/;
 
+	// Used to mark a function so it can be unwrapped.
 	var REF = '__extndmrk__';
  
 	// The base Class implementation (does nothing)
 	this.Class = function(){};
 
 	// Create a new Class that inherits from this class
-	Class.extnd = function(protoProps) {
+	Class.extnd = function (protoProps) {
 		var parent = this,
 			child,
+			prop,
 			_super = parent.prototype;
-		// Allow class inheritance
+		// Allow class inheritance.
 		if (typeof protoProps === 'function' && '__super__' in protoProps) {
-			// Copy parents prototype
+			// Copy parent's prototype.
 			var mixin = {}, attr;
 			for (attr in protoProps.prototype) {
 				if (attr !== 'constructor' &&
@@ -66,7 +44,7 @@
 		};
 
 		// Extend `extnd` and `__super__` into child.
-		for (var prop in parent) {
+		for (prop in parent) {
 			child[prop] = parent[prop];
 		}
 
@@ -80,41 +58,46 @@
 		// Add prototype properties (instance properties) to the subclass, if supplied.
 		if (protoProps) {
 			// Extend parent prototypes into child.
-			for (var prop in protoProps) {
+			for (prop in protoProps) {
 				child.prototype[prop] = protoProps[prop];
 			}
+
+			// A function wrapper which assigns this._super for the furation of the call,
+			// then marks the function so it can be unwrapped and re-wrapped for the next
+			// call which allows for proper multiple inheritance.
+			var functionWrapper = function (name, fn) {
+				var func = function class_super() {
+					var tmp = this._super;
+
+					// Add a new ._super() method that is the same method
+					// but on the super-class
+					this._super = _super[name] || unImplementedSuper(name);
+
+					// The method only need to be bound temporarily, so we
+					// remove it when we're done executing
+					var ret;
+					try {
+						ret = fn.apply(this, arguments);
+					} finally {
+						this._super = tmp;
+					}
+					return ret;
+				};
+				func[REF] = true;
+				return func;
+			};
 
 			// Copy the properties over onto the new prototype
 			for (var name in protoProps) {
 				// Check if we're overwriting an existing function
-				if (typeof protoProps[name] == 'function' &&  superTest.test(protoProps[name])) {
+				if (typeof protoProps[name] === 'function' &&  superTest.test(protoProps[name])) {
 					var fun = child.prototype[name];
 					if (fun[REF]) {
 						protoProps[name] = child.prototype.__unwrappedSuper__[name];
 					}
 
 					child.prototype.__unwrappedSuper__[name] = protoProps[name];
-					child.prototype[name] = (function(name, fn) {
-						var func = function class_super() {
-							var tmp = this._super;
-
-							// Add a new ._super() method that is the same method
-							// but on the super-class
-							this._super = _super[name] || unImplementedSuper(name);
-
-							// The method only need to be bound temporarily, so we
-							// remove it when we're done executing
-							var ret;
-							try {
-								ret = fn.apply(this, arguments);
-							} finally {
-								this._super = tmp;
-							}
-							return ret;
-						};
-						func[REF] = true;
-						return func;
-					})(name, protoProps[name]);
+					child.prototype[name] = functionWrapper(name, protoProps[name]);
 				}
 			}
 		}
@@ -125,33 +108,18 @@
 		return child;
 	};
 
-	// Some AMD build optimizers like r.js check for condition patterns like the following:
-	if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
-		// Expose Class to the global object even when an AMD loader is present in
-		// case Class is loaded with a RequireJS shim config.
-		// See http://requirejs.org/docs/api.html#config-shim
-		root.Class = Class;
-
-		// Define as an anonymous module so, through path mapping, it can be
-		// referenced as the "underscore" module
+	// Expose.
+	if (typeof exports !== 'undefined') {
+		// Node.js
+		exports.Class = Class;
+	} else if (typeof define !== 'undefined') {
+		// RequireJS
 		define(function() {
 			return Class;
 		});
-	}
-	// Check for `exports` after `define` in case a build optimizer adds an `exports` object
-	else if (freeExports && freeModule) {
-		// In Node.js or RingoJS
-		if (moduleExports) {
-			(freeModule.exports = Class).Class = Class;
-		}
-		// In Narwhal or Rhino -require
-		else {
-			freeExports.Class = Class;
-		}
-	}
-	else {
-		// In a browser or Rhino
-		root.Class = Class;
+	} else if (typeof window !== 'undefined') {
+		// Browser
+		window.Class = Class;
 	}
 
 }).call(this);
